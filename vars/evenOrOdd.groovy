53fcf0167b42adcf44d425a7e5d94b7884092386
omair-khan-roostify <42222955+omair-khan-roostify@users.noreply.github.com>
2018-08-21 11:38:39 -0700

    Update evenOrOdd.groovy

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -1,38 +1,29 @@
-
-def call(int buildNumber) {
-
-  if (buildNumber % 2 == 0) {
-    pipeline {
+def call() {
+    
+pipeline {
       agent any
+        environment {
+            AWS_ECR_LOGIN='true'
+            DOCKER_REGISTRY_URL='https://553655158632.dkr.ecr.us-west-1.amazonaws.com'
+            DOCKER_REGISTRY_CREDENTIALS='ecr:us-west-1:db812f2b-e8db-475c-ac6c-9981053e9bf5'
+            CUSTOM_SONAR_REPO_NAME=getRepoName()
+            //GIT_CREDENTIALS = credentials('GIT_SECRET_CREDENTIALS')
+        }
+    
       stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
-          }
+        stage ('checkout') {
+            steps {
+                    echo 'Checking out code from git repo'
+                    checkout scm
+                  }
         }
-        stage('build') {
-          steps {
-            sh "./gradlew clean build"
-          }
-        }
-      }
+        stage('Clean')
+            {
+                steps
+                    {
+                        sh "./gradlew -b roostify-product-pricing/build.gradle clean"
+                    }
+            }
+      }      
     }
   }
-    else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-        stage('build') {
-          steps {
-            sh "./gradlew clean build"
-          }
-        }
-      }
-    }
-  }
-}
