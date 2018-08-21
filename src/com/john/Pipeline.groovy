9f240b62dedbe5e0c4392edf87b66a93941e86fc
John Ruben <jruben@roostify.com>
2018-08-21 04:35:16 -0700

    adding to a class

--- src/com/john/Pipeline.groovy
+++ src/com/john/Pipeline.groovy
@@ -2,8 +2,30 @@ package com.john
 
 class Pipeline {
 
-  public Pipeline() {
-    // TODO Auto-generated constructor stub
+  def evenOrOdd(int buildNumber) {
+  if (buildNumber % 2 == 0) {
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
   }
+}
 
 }
